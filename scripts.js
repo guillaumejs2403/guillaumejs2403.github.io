7ff62ff708a671e9b806fd525d7bbb04438c1ff9
Guillaume Jeanneret <g.jeanneret10@uniandes.edu.co>
2020-11-26 18:01:14 -0500

    publications

--- scripts.js
+++ scripts.js
@@ -11,19 +11,21 @@ function closeNav() {
 function openProject(container, content, button, header){
 	document.getElementById(container).style.transitionDelay = '0s';
 	document.getElementById(content).style.transitionDelay = '0.5s';
-	document.getElementById(container).style.height = '500px';
+	document.getElementById(container).style.marginBottom = '1%';
+	document.getElementById(container).style.marginTop = '1%';
 	document.getElementById(content).style.width = "70%";
-	document.getElementById(content).style.border = "3px solid #ddd";
-	document.getElementById(button).innerHTML = "CLOSE " + header
+	document.getElementById(container).style.height = '410px';
+	document.getElementById(button).innerHTML = "Close"
 	document.getElementById(button).onclick = function() {closeProject(container, content, button, header)};
 }
 
 function closeProject(container, content, button, header){
 	document.getElementById(container).style.transitionDelay = '0.5s';
 	document.getElementById(content).style.transitionDelay = '0s';
+	document.getElementById(container).style.marginBottom = '0%';
+	document.getElementById(container).style.marginTop = '0%';
 	document.getElementById(content).style.width = "0";
-	document.getElementById(content).style.border = "0px solid #ddd";
 	document.getElementById(container).style.height = '0';
-	document.getElementById(button).innerHTML = "OPEN " + header
+	document.getElementById(button).innerHTML = header
 	document.getElementById(button).onclick = function() {openProject(container, content, button, header)};
 }
